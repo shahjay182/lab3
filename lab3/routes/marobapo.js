1d11be0e7e03d766a68e3277a75864cdf14fa02d
Jay Shah <200393613@student.georgianc.on.ca>
2019-10-01 21:07:37 -0400

    Add project files.

---
+++ lab3/routes/marobapo.js
@@ -0,0 +1,10 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('marobapo', { title: 'Maro Bapo', comment :'Jo mara bap uper nai javanu' });
+});
+
+module.exports = router;
